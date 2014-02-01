f50df699c05c6509b09a6ca824b406eb09d2adc3
jFox <jfoxsingularity@gmail.com>
2014-02-01 15:55:34 +0400

    Slim
    Перевёл весь интерфейс с чистого html на более практичный и чистый Slim.
    
    http://slim-lang.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starter::Application.config.secret_key_base = '099e5300af982b2c3870b37f71778aa98d8a3d6303eaeaf19713b307f53456449a99378dc8cb0c746bfbf04ea7210273a507a073868f6594197d44e6394c0a04'
